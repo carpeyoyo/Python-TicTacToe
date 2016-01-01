49caa88590f45fc76b3b6eb491ca14f9e64f83e0
Joshua Mazur <carpeyoyo@gmail.com>
2016-01-01 12:26:39 -0500

    Drawing board on canvas now works

--- TTT_gui.py
+++ TTT_gui.py
@@ -29,22 +29,15 @@ class TTT(object):
         # Label in button frame
         self.label_message = StringVar()
         Label(buttonframe,textvariable=self.label_message,font=("serif",18)).pack()
-        # Rawturtle
-        self.canvas = Canvas(frame,width=400,height=400,relief=SUNKEN)
+        # Canvas for drawing
+        self.width = 400
+        self.height = 400
+        self.canvas = Canvas(frame,width=self.width,height=self.height,relief=SUNKEN)
         self.canvas.grid()
-        #self.canvas.pack(side=TOP,fill=BOTH,expand=True)
-        self.canvas.pack(side=TOP)
-        self.turtle = turtle.RawTurtle(self.canvas) # embedded turtle.
-        self.turtle.speed(10000)
-        self.s = turtle.TurtleScreen(self.canvas) # Turtle's screen.
-        # Setting up initial center of board
-        self.x_center = 0
-        self.y_center = 0
-        self.maximum = 400
-        # Setting up response to clicks.
-        self.s.onclick(self.WhichSquare)
-        # Draws initial grid.
-        self.creategrid()
+        self.canvas.pack(side=TOP,fill=BOTH,expand=True)
+        # Setting event responses
+        self.canvas.bind("<Configure>",self.draw_board)
+        self.canvas.bind("<Button-1>",self.WhichSquare)
         # Initializes Game.
         self.g = game()
         self.label_message.set(self.g.message)
@@ -55,114 +48,83 @@ class TTT(object):
         Post: resets the turtle, draws new grid, and initiates new grid class.'''
         self.label_message.set("")
         self.canvas.delete("all")
-        self.turtle.reset()
-        self.turtle.speed(10000)
-        self.creategrid()
         self.g.newGame()
+        self.creategrid()
         self.label_message.set(self.g.message)
+
+    def draw_board(self,event):
+        '''Draws the tic tac toe board.
+        Pre: The event passed to the widget, when board needs redrawn.
+        Post: Board is drawn on window.'''
+        self.width = event.width
+        self.height = event.height
+        if self.width > self.height:
+            self.width = self.height
+        else:
+            self.height = self.width
+        self.canvas.delete(ALL)
+        self.creategrid()
 
     def creategrid(self):
         '''Draws the tictactow grid.
-        Pre: uses class's turtle object.
-        Post: draws lines on the turtle canvas by means of overlapping rectangles.'''
-        self.turtle.up()
-        self.turtle.goto(-150,150)
-        self.turtle.down()
-        self.turtle.fillcolor("gray")
-        self.turtle.begin_fill()
-        self.turtle.goto(-150,-150)
-        self.turtle.goto(150,-150)
-        self.turtle.goto(150,150)
-        self.turtle.goto(-150,150)
-        self.turtle.end_fill()
-        self.turtle.up()
-        self.turtle.goto(-150,50) # First Line
-        self.turtle.down()
-        self.turtle.fillcolor("black")
-        self.turtle.begin_fill()
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.end_fill()
-        self.turtle.up()
-        self.turtle.goto(-150,-50) # Second Line
-        self.turtle.down()
-        self.turtle.fillcolor("black")
-        self.turtle.begin_fill()
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.end_fill()
-        self.turtle.up()
-        self.turtle.goto(-50,150) # Third Line
-        self.turtle.down()
-        self.turtle.right(90)
-        self.turtle.fillcolor("black")
-        self.turtle.begin_fill()
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.end_fill()
-        self.turtle.up()
-        self.turtle.goto(50,150) # Forth Line
-        self.turtle.down()
-        self.turtle.fillcolor("black")
-        self.turtle.begin_fill()
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.forward(300)
-        self.turtle.left(90)
-        self.turtle.forward(2)
-        self.turtle.left(90)
-        self.turtle.end_fill()
-        self.turtle.up()
+        Pre: uses class's canvas object.
+        Post: draws lines on the canvas.'''
+        ## Drawling Background
+        self.canvas.create_rectangle(0,0,self.width,self.height,fill="grey")
+        ## Drawling Lines
+        # Line 1
+        self.canvas.create_line(self.width/3,0,self.width/3,self.height,width=4.0)
+        # Line 2
+        self.canvas.create_line(2*self.width/3,0,2*self.width/3,self.height,width=4.0)
+        # Line 3
+        self.canvas.create_line(0,self.height/3,self.width,self.height/3,width=4.0)
+        # Line 4
+        self.canvas.create_line(0,2*self.height/3,self.width,2*self.height/3,width=4.0)
 
-    def WhichSquare(self,x,y):
+    def WhichSquare(self,event):
         '''Determines which square has been clicked.
         Pre: x and y are the coordinates of the click.
         Post: initiates the takeTurn method from game class.'''
-        # Checking which square was clicked
-        if (x > 150) or (x < -150) or (y > 150) or (y < -150):
-            coor = "out" # If click was out of bounds.
-        elif y > 50: 
-            if x < -50:
-                coor = 0 # Top left square.
-            elif x < 50:
-                coor =  1 # Top middle square.
-            elif x < 150:
-                coor = 2 # Top Right square.
-        elif y > -50:
-            if x < -50:
-                coor = 3 # Middle left square.
-            elif x < 50:
-                coor = 4 # Center Square.
-            elif x < 150:
-                coor = 5 # Middle rigth square.
-        elif y > -150:
-            if x < -50:
-                coor = 6 # Bottom left square.
-            elif x < 50:
-                coor = 7 # Bottom Middle square.
-            elif x < 150:
-                coor = 8 # Bottom Right Square.
+        ## Checking which square was clicked
+
+        # Initial values
+        x = event.x
+        y = event.y
+        coor = "out"
+        first_x = self.width/3
+        second_x = (2*self.width)/3
+        third_x = self.width
+        first_y = self.height/3
+        second_y = (2*self.height)/3
+        third_y = self.height
+
+        # Determing square by coordinates
+        if (x > 0) and (y > 0):
+            if y < first_y:
+                if x < first_x:
+                    coor = 0 # Top left square.
+                elif x < second_x:
+                    coor =  1 # Top middle square.
+                elif x < third_x:
+                    coor = 2 # Top Right square.
+            elif y < second_y:
+                if x < first_x:
+                    coor = 3 # Middle left square.
+                elif x < second_x:
+                    coor = 4 # Center Square.
+                elif x < third_x:
+                    coor = 5 # Middle rigth square.
+            elif y < third_y:
+                if x < first_x:
+                    coor = 6 # Bottom left square.
+                elif x < second_x:
+                    coor = 7 # Bottom Middle square.
+                elif x < third_x:
+                    coor = 8 # Bottom Right Square.
+        print str(x) + "," + str(y)
+        print "Coordinate: " + str(coor)
         # Entering move in game
+        '''
         if coor != "out":
             if self.g.gameover == False:
                 current_sym = self.g.turn
@@ -174,56 +136,21 @@ class TTT(object):
                     self.label_message.set(self.g.message)
             else:
                 self.label_message.set("The game is over. Start a new game.")
+        '''
 
     def circle(self,center):
         '''Creates Circle on Board.
         Pre: Center of symbol.
         Post: Draws an O on the board at the center coordinate given.'''
         (x,y) = center
-        y_1 = y-25
-        y_2 = y-20
-        self.turtle.setheading(0)
-        self.turtle.up()
-        self.turtle.fillcolor("black")
-        self.turtle.goto(x,y_1)
-        self.turtle.begin_fill()
-        self.turtle.circle(25)
-        self.turtle.goto(x,y_2)
-        self.turtle.circle(20)
-        self.turtle.end_fill()
+        pass
 
     def cross(self,center):
         '''Creates Cross on board.
         Pre: Center is middle position of "X" on board.
         Post: Draws an X on the board at the center coordinate given'''
         (x,y) = center
-        x_1 = x - 25
-        x1 = x - 20
-        y_1 = y + 25
-        x_2 = x + 25
-        x2 = x + 20
-        y_2 = y - 25
-        self.turtle.goto((x_1,y_1))
-        self.turtle.up()
-        self.turtle.fillcolor("black")
-        self.turtle.begin_fill()
-        self.turtle.goto((x_1,y_1))
-        self.turtle.down()
-        self.turtle.goto((x1,y_1))
-        self.turtle.goto((x_2,y_2))
-        self.turtle.goto((x2,y_2))
-        self.turtle.goto((x_1,y_1))
-        self.turtle.end_fill()
-        self.turtle.up()
-        self.turtle.goto((x_1,y_2))
-        self.turtle.begin_fill()
-        self.turtle.down()
-        self.turtle.goto((x1,y_2))
-        self.turtle.goto((x_2,y_1))
-        self.turtle.goto((x2,y_1))
-        self.turtle.goto((x_1,y_2))
-        self.turtle.end_fill()
-        self.turtle.up()
+        pass
 
     def drawshape(self,coor,Turn):
         if coor == 0:            # Top Left
